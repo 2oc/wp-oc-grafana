26a3aed25eddb75e24d56c92ae52f0a5f1cfc2dc
Joeri van Dooren <ure@moreorless.be>
2016-06-27 07:12:48 +0200

    test

--- run.sh
+++ run.sh
@@ -43,11 +43,11 @@ fi
 
 #exec
 
-gosu grafana /usr/sbin/grafana-server  \
-  --homepath=/usr/share/grafana             \
-  --config=/etc/grafana/grafana.ini         \
-  cfg:default.paths.data="$GF_PATHS_DATA"   \
-  cfg:default.paths.logs="$GF_PATHS_LOGS"   \
-  cfg:default.paths.plugins="$GF_PATHS_PLUGINS"
+# gosu grafana /usr/sbin/grafana-server  \
+#   --homepath=/usr/share/grafana             \
+#   --config=/etc/grafana/grafana.ini         \
+#   cfg:default.paths.data="$GF_PATHS_DATA"   \
+#   cfg:default.paths.logs="$GF_PATHS_LOGS"   \
+#   cfg:default.paths.plugins="$GF_PATHS_PLUGINS"
 
 sleep 1d
